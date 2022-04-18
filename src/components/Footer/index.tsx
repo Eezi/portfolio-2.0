19f57e55ed51ec3f19ce1c7c83d8e038dafb2735
Eezi <tomi.a.salo@outlook.com>
2022-04-18 10:56:12 +0300

    Added flexbox blog post

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-
-    return (
-        <div className="hidden md:block text-primary text-center fixed bottom-0 inset-x-0 bg-mainBg text-sm font-mono p-5">
-           {'</>'} with passion by Tomi Salo © {new Date().getFullYear()}
-        </div>
-    );
-}
+  return (
+    <div className="hidden md:block text-primary text-center fixed bottom-0 inset-x-0 bg-mainBg text-sm font-mono p-5">
+      {'</>'} with passion by Tomi Salo © {new Date().getFullYear()}
+    </div>
+  );
+};
 
 export default Footer;
--- src/pages/Blogs/index.tsx
+++ src/pages/Blogs/index.tsx
@@ -1,17 +1,17 @@
-import React, { FC, ReactElement } from 'react'
+import React, { FC, ReactElement } from 'react';
 import Post from '../../components/Post';
 import { blogs } from '../../TextContent/texts';
 
-const Projects: FC<{}> = ():ReactElement => {
-    return (
-    <div>
-    <h3 className="text-center mb-5 md:text-3xl text-2xl">My posts</h3>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 content-center">
+const Projects: FC<{}> = (): ReactElement => {
+  return (
+    <div className="mb-28">
+      <h3 className="text-center mb-5 md:text-3xl text-2xl">My posts</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 content-center">
         {blogs.map((post) => (
-         <Post key={post.title} {...post} />
+          <Post key={post.title} {...post} />
         ))}
-    </div> 
+      </div>
     </div>
-    );
-}
+  );
+};
 export default Projects;
